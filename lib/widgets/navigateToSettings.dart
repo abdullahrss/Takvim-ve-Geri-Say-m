eafa447fa54c4689c02d0b16460824953c6078df
StatTark <33375656+StatTark@users.noreply.github.com>
2020-08-30 15:10:39 +0300

    Update navigateToSettings.dart

--- lib/widgets/navigateToSettings.dart
+++ lib/widgets/navigateToSettings.dart
@@ -1,7 +1,8 @@
 import 'package:ajanda/databasehelper/settingsHelper.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void navigateToSettingsDialog(context) {
+void navigateToSettingsDialog(context) async {
   var sdb = SettingsDbHelper();
   bool val = false;
   showDialog(
@@ -10,26 +11,53 @@ void navigateToSettingsDialog(context) {
         return StatefulBuilder(
           builder: (context, setState) {
             return AlertDialog(
+              title: Text("Dikkat!"),
               actions: <Widget>[
-                Text("ATTANTION PLS"),
-                Text("STAY BEHIND THE YELLOW LINE"),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                      "Sabit bildirim bazen düzgün çalışmayabilir. Düzgün çalışması için bilidirimleri açık olduğuna emin olun\n\nNot: Xiaomi telefonlarda uygulama ayarlarında Otomatik başlatma seçeneğinin açık olduğuna emin olun.",style: TextStyle(fontSize: 20),),
+                ),
                 Row(
+                  children: <Widget>[
+                    FlatButton(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Bir daha gösterme"),
+                      ),
+                      onPressed: () async {
+                        await sdb.updateWarning(val ? 1 : 0);
+
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right:8.0),
+                      child: Checkbox(
+                        value: val,
+                        onChanged: (v) => setState(() {
+                          val = v;
+                        }),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
                     FlatButton(
                       child: Text("Tamam"),
                       onPressed: () async{
-                        await sdb.updateWarning(val?1:0);
                         Navigator.pop(context);
                       },
                     ),
-                    Checkbox(
-                      value: val,
-                      onChanged: (v) => setState(() {
-                        val = v;
-                      }),
-                    ),
+                    FlatButton(
+                      child: Text("Ayarlara Git"),
+                      onPressed: () async{
+                        openAppSettings();
+                      },
+                    )
                   ],
-                ),
+                )
               ],
             );
           },
